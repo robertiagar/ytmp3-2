6c4476bffd9da1355eb11633704d38c582788a67
MikeD <mike@akagetsu.com>
2016-12-16 14:57:58 +0200

    Call server with link and save file

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, ResponseContentType } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+declare var saveAs: any;
 
 @Injectable()
 export class MainService {
 
-  constructor() { }
+  private serverUrl = 'http://localhost:3000/youtube/process_get';
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
+  constructor(private http: Http) { }
+
+  makeRequest(theLink: String): void {
+    const url = `${this.serverUrl}/?link=${theLink}`;
+
+    this.http.get(url, { responseType: ResponseContentType.ArrayBuffer })
+      .toPromise()
+      .then(response => {
+        let file = new Blob([response.blob()], {
+          type: 'audio/mpeg'
+        });
+        let title = JSON.parse(response.headers.get('content-disposition').split(';')[1].split('=')[1]);
+        saveAs(file, title);
+      });
+  }
 
 }
